e05cf9b9c409ffcc64fc7d42353bd4cf743f1066
Nazar Kalinchuk <nazariy2410@gmail.com>
2018-04-21 20:32:58 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
 
-app.get('/*', (req, res) => {
+app.post("/api/auth", (req, res)=>{
+   res.status(400).json({errors: {global: 'Invalid credentials'} });
+});
 
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
-app.listen(8080, () => console.log('Running on localhost'))
+app.listen(8080, () => console.log('Running on localhost'));
