88cbe8a7fcb40e0e5d47512b24278517e3d699ee
Nazar Kalinchuk <nazariy2410@gmail.com>
2018-04-22 15:14:27 +0300

    before user auth

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,15 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 
 const schema = new mongoose.Schema({
     email: { type: String, required:true, lowercase: true, index: true},
     passwordHash: { type: String , required: true }
 }, { timestamps: true });
+
+
+schema.methods.isValidPassword = function isValidPassword(password){
+    return bcrypt.compareSync(password, this.passwordHash);
+};
 
 export default mongoose.model('User', schema);
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.post('/', (req,res) => {
     const { credentials } = req.body;
     User.findOne({ email: credentials.email }).then(user =>{
-        if(user){
-
+        if(user && user.isValidPassword(credentials.password)){
+            res.json({ user: { email: user.email } });
         } else {
             res.status(400).json({ errors: {global: 'Invalid credentials'}});
         }
@@ -16,3 +16,4 @@ router.post('/', (req,res) => {
 });
 
 export default router;
+
