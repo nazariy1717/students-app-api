f30419851f4172d9689f0b15891266e569db7a25
Nazar Kalinchuk <nazariy2410@gmail.com>
2018-04-22 13:21:28 +0300

    auth route && user model

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+import auth from './routes/auth'
+
 
 const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/studentsDB');
 
 
-app.post("/api/auth", (req, res)=>{
-   res.status(400).json({errors: {global: 'Invalid credentials'} });
-});
+app.use('/api/auth', auth);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
---
+++ src/models/User.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+
+const schema = new mongoose.Schema({
+    email: { type: String, required:true, lowercase: true, index: true},
+    passwordHash: { type: String , required: true }
+}, { timestamps: true });
+
+export default mongoose.model('User', schema);
